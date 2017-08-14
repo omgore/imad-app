7b78f791ae18ff20129476d1dcfcdf29656bb79a
omgore <omgore2@gmail.com>
2017-08-14 22:50:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -18,11 +18,11 @@ app.get('/article-one', function (req, res) {
 });
 
 app.get('/article-two', function (req, res) {
-  res.send("This is article two");
+  res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
 
 app.get('/article-three', function (req, res) {
-  res.send("This is article three");
+  res.sendFile(path.join(__dirname, 'ui', 'article-thre.html'));
 });
 
 app.get('/ui/madi.png', function (req, res) {
