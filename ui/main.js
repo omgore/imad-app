443032e3287efb48fa0d6bef0fc6c22d446e1a9c
omgore <omgore2@gmail.com>
2017-08-15 12:52:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,9 @@ console.log('Loaded!');
 
 var element = document.getElementById('main-text');
 element.innerHTML = 'New value';
+
+//move image madi
+var img = document.getElementById('madi');
+img.onClick =  function () {
+  img.style.marginLeft = '100 px';  
+};
