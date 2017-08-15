4061d5b03f7ad07cf3056ebaa414d6c1fe849b34
omgore <omgore2@gmail.com>
2017-08-15 13:09:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,6 +6,6 @@ element.innerHTML = 'New value';
 //move image madi
 var img = document.getElementById('madi');
 img.onClick =  function () {
-    consple.log('in on click funtion of image madi');
-  img.style.marginLeft = '100 px';  
+    console.log('in on click funtion of image madi');
+    img.style.marginLeft = '100 px';  
 };
