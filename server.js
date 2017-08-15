8ccf9efa371e9cf0c78c7bcb53290223973df0a7
omgore <omgore2@gmail.com>
2017-08-15 12:40:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -42,10 +42,10 @@ var articles= {
 
 function createTemplate(data)
 {
-    var title=data.title;
-    var heading=data.heading;
-    var date=data.date;
-    var content=data.content;
+    var title = data.title;
+    var heading = data.heading;
+    var date = data.date;
+    var content = data.content;
     var htmlTemplate =
     `
     <html>
