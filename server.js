31cd67a41f2b3b139850731bd35cd79595ac8910
omgore <omgore2@gmail.com>
2017-08-16 22:13:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -91,7 +91,9 @@ app.get('/ui/style.css', function (req, res) {
 });
 
 app.get('/:articleName', function (req, res) {
+    
     var articleName = req.params.articleName;
+    console.log(articleName);
     res.send(createTemplate(articles[articleName]));
 });
 
