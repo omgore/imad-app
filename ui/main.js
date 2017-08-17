95a02c818b1229f3e7fb92af7a7b59f54af6f4c9
omgore <omgore2@gmail.com>
2017-08-17 22:35:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,3 +9,19 @@ console.log('Loaded!');
 //     console.log('in on click funtion of image madi');
 //     img.style.marginLeft = '100 px';  
 // };
+
+
+//counter code
+var counter = 0;
+var button = document.getElementById('counter');
+button.onclick = function () {
+  //make request to counter endpoint
+  
+  //capture the respnse and store to variable
+  
+  //render the variable to span
+  
+  counter = counter + 1;
+  var span = document.getElementById('count');
+  span.innerHTML = counter.toString();
+};
