804514461b4a81416d6e9880ce0c78ac94544c9e
omgore <omgore2@gmail.com>
2017-08-19 13:15:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -85,6 +85,11 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/:articleName', function (req, res) {
+    var articleName = req.params.articleName;
+    res.send(createTemplate(articles[articleName]));
+});
+
 var counter = 0;
 app.get('/counter', function(req,res) {
     counter = counter + 1;
@@ -95,11 +100,6 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/:articleName', function (req, res) {
-    var articleName = req.params.articleName;
-    res.send(createTemplate(articles[articleName]));
-});
-
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
@@ -107,6 +107,17 @@ app.get('/ui/madi.png', function (req, res) {
 
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
+var names =[];
+app.get('/submit-name/:name',function(req,res){
+    //get the name from request
+    var name = req.params.name;
+    
+    names.push(name);
+    //JSON : java script object notation
+    
+    res.send(JSON.stringify(names));
 });
 
 // Do not change port, otherwise your app won't run on IMAD servers
