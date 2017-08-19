b3b2eca37c866587678018ac02cdbd41f0f795f3
omgore <omgore2@gmail.com>
2017-08-19 13:29:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -39,8 +39,6 @@ button.onclick = function () {
 
 };
 
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
 
 var submit = document.getElementById('submit');
 submit.onclick = function () {
@@ -72,7 +70,8 @@ submit.onclick = function () {
   };
   
   //make request to counter endpoint
-  
+    var nameInput = document.getElementById('name');
+    var name = nameInput.value;
   request.open('GET','http://omgore2.imad.hasura-app.io/submit-name?name=' + name, true);
   request.send(null);
   
