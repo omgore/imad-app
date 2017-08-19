07fe2ee9a48db8a0d2c80a36ba7ab6d7e67cd086
omgore <omgore2@gmail.com>
2017-08-19 13:21:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -110,9 +110,9 @@ app.get('/ui/main.js', function (req, res) {
 });
 
 var names =[];
-app.get('/submit-name/:name',function(req,res){
+app.get('/submit-name',function(req,res){
     //get the name from request
-    var name = req.params.name;
+    var name = req.query.name;
     
     names.push(name);
     //JSON : java script object notation
